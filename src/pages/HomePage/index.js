ce98fe7d5a36e41a005e90a2b11b5615fab79015
Francisco Santana Cardoso <fsantanagy@gmail.com>
2023-12-29 14:58:49 -0300

    created repository layer

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,4 +1,13 @@
+import {useEffect, useState} from 'react';
+import { getMoviesList } from '../../services/repository';
 function Home() {
+    const[movies, setMovies] = useState([]);
+
+    useEffect(()=>{
+        setMovies (getMoviesList('1'))
+    },[])
+
+
     return(
         <h1>Home page</h1>
     );
---
+++ src/services/repository.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+// URL base: https://api.themoviedb.org/3
+// API key: api_key=146e18d192e063b3f30d264301c8cb27
+// Complement: movie/now_playing?language=pt-br&page=1&
+const BaseApi = axios.create({
+    baseURL: 'https://api.themoviedb.org/3/'
+});
+
+
+export async function getMoviesList(page) {
+
+    const response = await BaseApi.get("movie/now_playing", {
+        params:{ 
+            api_key: "146e18d192e063b3f30d264301c8cb27",
+            language: "pt-BR",
+            page: page
+        }
+    });
+
+    return response
+}
